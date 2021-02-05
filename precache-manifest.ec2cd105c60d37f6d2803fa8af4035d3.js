4b589725f1486604e322836d28b97a63a20c9ca9
Byron Blank <byronbla@gmail.com>
2021-02-04 21:17:42 -0500

    first commit

---
+++ precache-manifest.ec2cd105c60d37f6d2803fa8af4035d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d24d30d4891e205d36d3995e352b074",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94c0c6e976e545d0d561",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "37bf5af0964265cf96ac",
+    "url": "/static/css/main.06d1c424.chunk.css"
+  },
+  {
+    "revision": "94c0c6e976e545d0d561",
+    "url": "/static/js/2.9a1a309b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a1a309b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37bf5af0964265cf96ac",
+    "url": "/static/js/main.b9e7a8c4.chunk.js"
+  },
+  {
+    "revision": "38041332d7552b4e4323",
+    "url": "/static/js/runtime-main.d3573f76.js"
+  }
+]);
